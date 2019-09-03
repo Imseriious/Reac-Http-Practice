741549093083eba9bc39041ef1feeed6cd14359d
Eduard Samuel <samueledi80@hotmail.com>
2019-09-03 18:15:22 +0200

    interceptors added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(requestConfig => {
+    console.log(requestConfig);
+    return requestConfig; //We always need to return so we can use it locally from components
+}, error => {
+    console.log(error);
+    return Promise.reject(error); //Need to return
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response; //We always need to return so we can use it locally from components
+}, error => {
+    console.log(error);
+    return Promise.reject(error); //Need to return
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
